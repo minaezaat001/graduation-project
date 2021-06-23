a61cd9e6773f98e06739e37e4ae46f6335d76988
minaezaat001 <59961165+minaezaat001@users.noreply.github.com>
2021-06-23 04:27:24 +0200

    minamina

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/', function () {
-    return view('welcome2');
-});
-Route::get('/', function () {
-    return view('welcome3');
-});
-Route::get('/', function () {
-    return view('welcome4');
-});
-Route::get('/', function () {
-    return view('welcome55');
-});
